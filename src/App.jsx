32111310e3c5206804707c7b75db574f219ce98f
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-06-01 23:54:43 -0300

    create pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from "styled-components"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from "axios";
+import React from "react";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import TodayPage from "./pages/TodayPage";
+import HabitsPage from "./pages/HabitsPage";
+import HistoricPage from "./pages/HistoricPage";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/cadastro" element={<RegisterPage />} />
+        <Route path="/hoje" element={<TodayPage />} />
+        <Route path="/habitos" element={<HabitsPage />} />
+        <Route path="/historico" element={<HistoricPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
