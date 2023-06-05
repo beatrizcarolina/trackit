fdc964db2a4de9557d790237c53af45e8468be62
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-06-05 01:09:46 -0300

    create HabitsPage

--- src/pages/HabitsPage.jsx
+++ src/pages/HabitsPage.jsx
@@ -4,14 +4,35 @@ import axios from "axios";
 import { AuthContext } from "../components/AuthContext";
 import NavBar from "../components/NavBar";
 import Menu from "../components/Menu";
+import NewHabit from "../components/NewHabit";
+import Habit from "../components/Habit";
 
 
 export default function HabitsPage() {
     const { token, login, logout } = React.useContext(AuthContext);
     const [ habits, setHabits] = React.useState([]);
-
+    const [ addNewHabit, setAddNewHabit] = React.useState(false);
+    const [ habitName, setHabitName ] = React.useState("");
+    const [ days, setDays ] = React.useState([]);
+    
     React.useEffect(() => {
-		axios
+        if(token) {
+            getHabits();
+        }
+		
+	}, []);
+
+
+    function toggleNewHabit() {
+        setAddNewHabit(!addNewHabit);
+    }
+
+    function getHabits() {
+        cancelHabit();
+        setDays([]);
+        setHabitName("");
+
+        axios
             .get("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits", {
                 headers: {
                   Authorization: `Bearer ${token.token}`,
@@ -21,10 +42,11 @@ export default function HabitsPage() {
             .then(response => {setHabits(response.data)})
 
             .catch((error) => console.log(error));
-	}, []);
+    }
 
-    console.log(token);
-    console.log(habits);
+    function cancelHabit() {
+        setAddNewHabit(false);
+    }
 
 
     return(
@@ -32,10 +54,38 @@ export default function HabitsPage() {
             <NavBar></NavBar>
 
             <Main>
-                <h3>Meus hábitos</h3>
-                {habits.length == 0 &&
-                    <h1>Você não tem nenhum hábito cadastrado ainda. 
-                        Adicione um hábito para começar a trackear!</h1>}
+                <Header>
+                    <h3>Meus hábitos</h3>
+                    <button onClick={toggleNewHabit} data-test="habit-create-btn">+</button>
+                </Header>
+                {   
+                    addNewHabit &&
+                    <NewHabit
+                        cancelHabit={cancelHabit}
+                        updateHabits={getHabits}
+                        days={days}
+                        setDays={setDays}
+                        habitName={habitName}
+                        setHabitName={setHabitName}
+                    ></NewHabit>
+                }
+                {
+                    habits.length == 0 &&
+                        <h1>Você não tem nenhum hábito cadastrado ainda. 
+                        Adicione um hábito para começar a trackear!</h1>
+                }
+                {
+                    habits.length > 0 &&
+                        habits.map((habit, index) => (
+                            <Habit
+                                key={index}
+                                habit={habit}
+                                updateHabits={getHabits}
+                            ></Habit>
+                            
+                        ))
+                }
+
             </Main>
 
             <Menu></Menu>
@@ -50,22 +100,41 @@ const HabitsContainer = styled.div`
     justify-content: center;
 `
 const Main = styled.div`
-   
+    width: 375px;
+    h1{
+        height: 74px;
+        margin-left: 15px;
+        font-size: 18px;
+        line-height: 22px;
+        color: #666666;
+    }
+`
+
+const Header = styled.div`
+    height: 35px;
+    display: flex;
+    justify-content: space-between;
+    align-items: end;
+    margin-top: 98px;
+    margin-bottom: 10px;
+
     h3{
-        heigth: 29px;
         margin-left: 17px;
-        margin-top: 98px;
         font-size: 26px;
         line-height: 29px;  
         color: #126BA5;   
     }
 
-    h1{
-        height: 74px;
-        margin-left: 15px;
-        margin-top: 17px;
-        font-size: 18px;
-        line-height: 22px;
-        color: #666666;
+    button{
+        width: 40px;
+        height: 35px;
+        background-color: #52B6FF;
+        border-radius: 4.63636px;
+        font-size: 27px;
+        line-height: 34px;
+        text-align: center;
+        color: #FFFFFF;
+        position: relative;
+        margin-right: 17px;
     }
 `
